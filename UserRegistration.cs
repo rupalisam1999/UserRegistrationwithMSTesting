7c453c1dedffb3758495c0ce793ea1e657c2cc7b
rupalisam1999 <rupalisangale1999@gmail.com>
2022-03-06 13:45:09 +0530

    [Rupali]ADDED: Validate Last name with MS Testing

--- UserRegistration.cs
+++ UserRegistration.cs
@@ -10,10 +10,23 @@ namespace UserRegistrationWithMSTesting
     public class UserRegistration
     {
         const string FIRST_NAME_PATTERN = "^[A-Z]{1}[a-z]{3,}$";
+        const string LAST_NAME_PATTERN = "^[A-Z]{1}[a-z]{3,}$";
 
         public bool ValidateFirstName(string firrstName)
         {
             var result = Regex.Match(firrstName, FIRST_NAME_PATTERN);
+            if (result.Success)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public bool ValidateLatName(string LastName)
+        {
+            var result = Regex.Match(LastName, LAST_NAME_PATTERN);
             if (result.Success)
             {
                 return true;
